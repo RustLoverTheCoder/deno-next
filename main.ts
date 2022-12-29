6a213183db27f990f9fdf4d5558abe138ae38193
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2022-12-29 11:30:45 +0800

    feat: add importMap

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 export function add(a: number, b: number): number {
-  return a + b;
+  return a + b + a + a;
 }
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
---
+++ packages/server/types.ts
@@ -0,0 +1,62 @@
+export type { ServeInit } from "https://deno.land/std@0.150.0/http/server.ts";
+
+export type StartOptions = FreshOptions & {
+  experimentalDenoServe?: boolean;
+};
+
+export interface FreshOptions {
+  render?: RenderFunction;
+  plugins?: Plugin[];
+  staticDir?: string;
+}
+
+export type RenderFunction = (
+  ctx: RenderContext,
+  render: InnerRenderFunction
+) => void | Promise<void>;
+
+export type InnerRenderFunction = () => string;
+
+export class RenderContext {
+  #id: string;
+  #state: Map<string, unknown> = new Map();
+  #styles: string[] = [];
+  #url: URL;
+  #route: string;
+  #lang: string;
+
+  constructor(id: string, url: URL, route: string, lang: string) {
+    this.#id = id;
+    this.#url = url;
+    this.#route = route;
+    this.#lang = lang;
+  }
+
+  get id(): string {
+    return this.#id;
+  }
+
+  get state(): Map<string, unknown> {
+    return this.#state;
+  }
+
+  get styles(): string[] {
+    return this.#styles;
+  }
+
+  get url(): URL {
+    return this.#url;
+  }
+
+  get route(): string {
+    return this.#route;
+  }
+
+  get lang(): string {
+    return this.#lang;
+  }
+
+  set lang(lang: string) {
+    this.#lang = lang;
+  }
+}
